5dc1db81157fd2e4bbb98769934636a9031971b9
Elias Achi <elias@plazacybersecurity.com>
2023-07-05 13:47:04 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# RSKMailer
+
+A simple API to receive request to send mail using a SMTP server or oAUTH2 (Gmail, Outlook, etc.)
+
+## Features
+
+- Simple API to send mail
+- Have an option to allow send from specific domains only.
+- Full log of events (access, errors, etc.)
+
+## Prerequisites
+
+- Docker and Docker Compose(as docker plugin) fully configured.
+- Any SMTP server or oAUTH2 (Gmail, Outlook, etc.) account.
+- A website that uses some kind of form to send mail that has the following fields:
+  - `name`
+  - `email`
+  - `subject`
+  - `message`
+
+## Installation
+
+- Git clone the repo and install the dependencies:
+- Edit docker-compose.yml and change the environment variables to your needs. Check `docker-compose.sample.yml` for more details and guidance. This variables, following the details explained in the *Prerequisites* section will be as follows:
+
+```yaml
+    environment:
+      TZ : America/New_York
+      APP_PORT : 3001 #
+      APP_DEBUG : 0 # This is the debug level for the app. 0 is no debug, 1 is debug.
+      MAIL_TRANSPORTER : |
+                            {
+                              "host" : "localhost",
+                              "port" : 25,
+                              "secure" : false
+                            }
+      MAIL_DEFAULT_RECEIVER : myfriend@mail.com
+      MAIL_SENDER : mymail@mail.com
+      MAIL_DEFAULT_SUBJECT : Hello from RSK Mailer
+```
+
+- Run `docker compose up -d` to start the container application
+
---
+++ main.js
@@ -0,0 +1,142 @@
+// RSKMailer
+// (c) 2023, Raskitoma.io
+
+// Simple node program to allow mail send from a web page
+const dotenv = require('dotenv');
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require('nodemailer');
+const moment = require('moment');
+const app = express();
+const host = '0.0.0.0'
+const app_name = 'RSKMailer'
+
+// load environment variables
+console.log('Loading environment variables...');
+dotenv.config();
+const port = process.env.APP_PORT || 3001;
+const appdebug = process.env.APP_DEBUG || '0'; // default to false
+const cors_origin = process.env.CORS_ORIGIN || '*';
+const mail_transporter_str = process.env.MAIL_TRANSPORTER || `{ "service": "gmail", "auth": { "user": "youremail@gmail.com", "pass": "yourpassword" } }`; 
+const mail_default_receiver = process.env.MAIL_DEFAULT_RECEIVER || 'myfriend@yahoo.com';
+const mail_sender = process.env.MAIL_SENDER || 'youremail@gmail.com';
+const mail_default_subject = process.env.MAIL_DEFAULT_SUBJECT || 'Hello from RSKMailer';
+console.log('Environment variables loaded.');
+
+// Setting up logger
+function applogger(message, module='MSG', severity='info', extras='', request=null, response=null) {
+    let time_now = moment().format();
+    let method = 'SCRIPT';
+    let browser = 'SCRIPT';
+    let uri = 'SCRIPT';
+    let ip = 'SCRIPT';
+    let status_code = 0;
+    if (request) {
+        method = request.method || 'SCRIPT';
+        browser = request.rawHeaders[15] || 'SCRIPT';
+        uri = request.url || 'SCRIPT';
+        ip = request.headers["x-forwarded-for"] || request.socket.remoteAddress || 'SCRIPT';
+    }
+    if (response) {
+        if (response.statusCode) {
+        status_code = response.statusCode;
+        } else if (response.status) {
+        status_code = response.status;
+        }
+    }
+    let log_msg = `${time_now} | [${app_name}]-[${module}][${method}][${status_code}][${message}][${uri}][${ip}][${browser}][${extras}]`;
+    if (severity == 'error') {
+        console.error(log_msg);
+    } else if (severity == 'warn') {
+        console.warn(log_msg);
+    }  else {
+        console.log(log_msg);
+    }
+}
+  
+// cors fix
+let allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', `${cors_origin}`);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', '*');
+    next();
+};
+
+// set up server
+console.log('Setting up...');
+app.use(allowCrossDomain);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+const is_debug = appdebug=='1' ? true : false;
+
+// Setting up mail transporter
+console.log('Setting up mail transporter...');
+const mail_transporter = JSON.parse(mail_transporter_str);
+const transporter = nodemailer.createTransport(mail_transporter);
+
+// Setting up default mail options
+
+const defaultMailOptions = {
+    from: mail_sender,
+    to: mail_default_receiver,
+    subject: mail_default_subject,
+    text: 'Hello from RSKMailer',
+};
+
+// *********************************************************************************************************************
+// APP Routes
+  console.log('Prepping app routes...')
+// Root route
+app.get('/', (req, res) => {
+  applogger('Client Request', 'REQUEST', 'info', '', req, res);
+  res.send('RSKMailer - System Live & Listening');
+});
+
+app.post('/contact', (req, res) => {
+    // if the requests doesn't comes from the allowed origins, return error
+    if (cors_origin != '*' && cors_origin != req.headers.origin) {
+        let error_msg = `Request from ${req.headers.origin} not allowed`;
+        applogger('Request from not allowed origin', 'REQUEST', 'error', error_msg, req, res);
+        // return error_msg to the client with status code 403
+        res.send(error_msg).status(403);
+    }
+
+    applogger('Contact Request', 'CONTACT', 'info', '', req, res);
+    let mailname = req.body.name || '';
+    let mailfrom = req.body.email || mail_sender;
+    let mailmsg = req.body.message || '';
+    let mailsubject = req.body.subject || mail_default_subject;
+
+    let mailsbj = `Contact Request: ${mailsubject}`;
+    let mailtxt = `From: ${mailname}<br>Email: ${mailfrom}<br><br>${mailmsg}`;
+
+    const mailOptions = {
+        from: mailfrom,
+        to: mail_default_receiver,
+        subject: mailsbj,
+        html: mailtxt,
+        text: mailtxt,
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            let error_msg = `Mail Send Error: ${error}`;
+            applogger('Send Mail Error', 'MAILER', 'error', error_msg, req, res);
+            // return error_msg to the client with status code 500
+            res.send(error_msg).status(500);
+
+        } else {
+            let success_msg = `Mail Sent: ${info.response}`;
+            applogger('Send Mail Success', 'MAILER', 'info', success_msg, req, res);
+            // return success_msg as json with key 'status' value as 'sent' to the client with status code 200
+            res.json({status: 'sent', message: success_msg}).status(200);
+        }
+    });
+});
+
+console.log('Starting up server...')
+app.listen(port, host, () => {
+  const time_now = moment().format();
+  console.log(`${app_name} - System Live & Listening on http://${host}:${port} ======: ${time_now} :=====`);
+});
+
